246355d6f8c8efc69aa785dfc93f074329cd86a2
Aymen Kadri <aymenkadri798@gmail.com>
2022-10-23 10:38:28 +0200

    selection du mot aleatoire

--- src/Lemot.java
+++ src/Lemot.java
@@ -1,2 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Lemot {
+    public static String mot_aleatoire() throws FileNotFoundException
+    {
+        File f = new File("./Mots.txt");
+        String result = null;
+        Random rand = new Random();
+        int n = 0;
+        for(Scanner sc = new Scanner(f); sc.hasNext(); )
+        {
+            ++n;
+            String line = sc.nextLine();
+            if(rand.nextInt(n) == 0)
+                result = line;
+        }
+
+        return result;
+    }
 }
