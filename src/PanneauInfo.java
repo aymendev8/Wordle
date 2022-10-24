fe30ffa710dec77107c114aaba219a43a2003f04
Aymen Kadri <aymenkadri798@gmail.com>
2022-10-24 16:09:49 +0200

    Modif du texte dans le JLABEL

--- src/PanneauInfo.java
+++ src/PanneauInfo.java
@@ -7,7 +7,7 @@ public class PanneauInfo extends JPanel {
     public PanneauInfo(){
         this.setBackground(Color.white);
         this.setPreferredSize(new Dimension(500,30));
-        info = new JLabel("Wordle");
+        info = new JLabel("Ecris ton mot ! ");
         info.setForeground(Color.green);
         info.setFont(new Font("Verdana",1,15));
         this.add(info);
