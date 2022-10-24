0df5c19cfab54262e284d842d007832b0fe5cf37
Aymen Kadri <aymenkadri798@gmail.com>
2022-10-24 16:09:13 +0200

    V1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.io.FileNotFoundException;
+
 public class Main {
     public static Fenetre f;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         f = new Fenetre();
         f.setVisible(true);
         System.out.println("La fenetre est lancée ");
